ac2cad045a2850fe90938f891220a21441f91e2f
Andres Pérez López <contact@andresperezlopez.com>
2018-02-25 23:28:02 +0100

    small os path change for docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 import six
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
